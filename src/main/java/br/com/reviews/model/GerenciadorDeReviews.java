de6978d8d82326c03a4458ad2544697a004f5ee5
Leo Silva <Leo.leomota@gmail.com>
2016-07-27 22:40:47 -0300

    criando o metodo validaVoto para encapsular os ifs

--- src/main/java/br/com/reviews/model/GerenciadorDeReviews.java
+++ src/main/java/br/com/reviews/model/GerenciadorDeReviews.java
@@ -14,39 +14,48 @@ public class GerenciadorDeReviews {
 
 	@Autowired
 	private ReviewsDao reviewsDao;
-	
+
 	@Autowired
 	private UsuarioDao usuarioDao;
 
 	@Autowired
 	private AppDao appDao;
-	
-	//TODO nao permitir que 2 pessoas deem review no mesmo app(lock) ao mesmo tempo
+
+	// TODO nao permitir que 2 pessoas deem review no mesmo app(lock) ao mesmo tempo
 	public boolean addReviews(Usuario usuarioQueVota, App app) {
 
 		Usuario usuarioVotado = app.getUsuario();
-		
-		//TODO melhorar esses ifs
-		if(!usuarioVotado.podeSerVotado()){
-			return false;
+
+		if (validaVoto(usuarioQueVota, usuarioVotado)) {
+
+			usuarioQueVota.vota(app);
+
+			usuarioDao.altera(usuarioQueVota);
+			usuarioDao.altera(usuarioVotado);
+			appDao.altera(app);
+
+			return true;
 		}
-		if(usuarioQueVota.equals(usuarioVotado)){
-			return false;
-		}
 		
-		usuarioQueVota.vota(app);
-		
-		usuarioDao.altera(usuarioQueVota);
-		usuarioDao.altera(usuarioVotado);
-		appDao.altera(app);
-		
-		return true;
-		
+		return false;
+
 	}
-	
-	public List<App> getFilaReviews(){
+
+	private boolean validaVoto(Usuario usuarioQueVota, Usuario usuarioVotado) {
+
+		if (usuarioQueVota.equals(usuarioVotado)) {
+			return false;
+		}
+		if (!usuarioVotado.podeSerVotado()) {
+			return false;
+		}
+		return true;
+	}
+
+	public List<App> getFilaReviews() {
+
 		return reviewsDao.getListaParaReview();
-		//TODO fazer historico de votos para que o app já votado não apareça na lista novamente
-		//TODO nao listar apps do proprio usuario
+		// TODO fazer historico de votos para que o app já votado não apareça na lista novamente
+		// TODO nao listar apps do proprio usuario
 	}
 }
